32c01ad29d230ee4c6fbfa3a8207988464968657
Brandon Garate <brandongarate177@gmail.com>
2024-07-12 01:54:26 -0700

    some big changes coming fr

--- main.py
+++ main.py
@@ -35,19 +35,42 @@ class SoftwareRender:
     # im feeling big brain type shiiit 
 
     def create_objects(self):
-        self.camera = Camera(self, [0.5, 1, -4])
+        self.camera = Camera(self, [-5, 5, -50])
         self.projection = Projection(self)
-        self.object = Object3D(self)
-        self.object.translate([0.2, 0.4, 0.2])
-        self.object.rotate_y(math.pi / 6)
+        self.object = self.get_object_from_file('resources/t_34_obj.obj')
+        # self.object = Object3D(self)
+        # self.object.translate([0.2, 0.4, 0.2])
+        # self.object.rotate_y(math.pi / 6)
+
+        # self.axes = Axes(self)
+        # self.axes.translate([0.7, 0.9, 0.7])
+        # self.world_axes= Axes(self)
+        # self.world_axes.movement_flag = False
+        # self.world_axes.scale(2.5)
+        # self.world_axes.translate([0.0001, 0.0001, 0.0001])
+
+    def get_object_from_file(self, filename):
+            vertex, faces = [], []
+            with open(filename) as f:
+                for line in f:
+                    if line.startswith('v '):
+                        vertex.append([float(i) for i in line.split()[1:]] + [1])
+                    elif line.startswith('f'):
+                        faces_ = line.split()[1:]
+                        faces.append([int(face_.split('/')[0]) - 1 for face_ in faces_])
+            return Object3D(self, vertex, faces)    
+
 
     def draw(self):
         self.screen.fill(pg.Color('darkslategray'))
+        # self.axes.draw()
+        # self.world_axes.draw()
         self.object.draw()
 
     def run(self): 
         while True:
             self.draw()
+            self.camera.control()
             [exit() for i in pg.event.get() if i.type == pg.QUIT]
             pg.display.set_caption(str(self.clock.get_fps()))
             pg.display.flip()
